6c9e4e187e18596a000c92acb00db6d1f26ace8b
Jérémie Dubuis <jeremie.dubuis@gmail.com>
2022-03-02 17:20:33 +0100

    :speaker_loud:

--- helpers/validateOrigin.ts
+++ helpers/validateOrigin.ts
@@ -1,14 +1,14 @@
-import type { Request } from "polka";
-import { ApiError } from "../api/ApiError";
+import type { Request } from 'polka';
+import { ApiError } from '../api/ApiError';
 
 export const validateOrigin = (req: Request, ignoreOrigin?: boolean) => {
-  if (ignoreOrigin) return;
+    if (ignoreOrigin) return;
 
-  if (
-    req.headers.origin &&
-    req.headers.origin !== process.env.HOST &&
-    !process.env.CORS?.includes(req.headers.origin)
-  ) {
-    throw new ApiError("Unauthorized", 401);
-  }
+    if (
+        req.headers.origin &&
+        req.headers.origin !== process.env.HOST &&
+        !process.env.CORS?.includes(req.headers.origin)
+    ) {
+        throw new ApiError('Unauthorized: ' + req.headers.origin, 401);
+    }
 };
--- helpers/validateSession.ts
+++ helpers/validateSession.ts
@@ -1,8 +1,8 @@
-import type { Request } from "$types/server";
-import { ApiError } from "../api/ApiError";
+import type { Request } from '$types/server';
+import { ApiError } from '../api/ApiError';
 
 export const validateSession = (req: Request, allowDisconnected?: boolean) => {
-  if (!allowDisconnected && !req.session) {
-    throw new ApiError("Unauthorized", 401);
-  }
+    if (!allowDisconnected && !req.session) {
+        throw new ApiError('Unauthorized session', 401);
+    }
 };
